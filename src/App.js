821b8c9b0e5c1c2841751cc9ad1e8de899128fc6
Justyna Gładysz <jus.gladysz@gmail.com>
2022-01-13 14:55:35 +0100

    Update navigation styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {routes} from "./routes";
-import Navigation from "./components/shared/Navigation/Navigation"
+import Navigation from "./components/shared/Navigation/Navigation.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/scss/custom.scss'
 
 function App() {
     return (
         <>
-            {/*<Header />*/}
             <BrowserRouter>
                 <Navigation/>
                 <Routes>
