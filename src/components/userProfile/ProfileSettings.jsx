82767086a27ded1c2a5700a3d326014e8537defa
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2022-01-15 09:41:18 +0100

    Basic ProfileSetting component created

---
+++ src/components/userProfile/ProfileSettings.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Button } from 'react-bootstrap';
+
+// import user from '../../assets/images/user-circle-solid.png';
+// import edit from '../../assets/images/edit-solid.png';
+
+function ProfileSettings () {
+    return (
+        <>
+            <h2>Settings ⚙️</h2>
+        </>
+    );
+}
+
+export default ProfileSettings;
