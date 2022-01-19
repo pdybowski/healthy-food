3d6bfe0d9469409525ef1c1629b4e31dd8a00d20
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2022-01-19 18:47:19 +0100

    Modal Added to Registration

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button'
 
-const Registration = () => {
-    return <div></div>;
+const Registration = ({ props }) => {
+    return (
+        <Modal {...props} size='lg' aria-labelledby='contained-modal-title-vcenter' centered>
+            <Modal.Header closeButton>
+                <Modal.Title id='contained-modal-title-vcenter'>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div>
+                    <div>I will put here Sign In</div>
+                    <div>I will put here Create Account</div>
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
 };
 
 export default Registration;
