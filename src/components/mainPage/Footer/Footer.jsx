daaa20c6dd9f914d80872288c1ad54097e3eb987
Tomasz <przadkatomasz.alt@gmail.com>
2022-01-19 01:14:57 +0100

    create Footer and add it to landing page

---
+++ src/components/mainPage/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <>
+            <footer className="py-2 my-2">
+                <p className="text-center text-muted">(c) Healty Food 2022</p>
+            </footer>
+        </>
+    );
+};
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,6 @@
 import Banner from '../../components/mainPage/Banner/Banner.jsx';
 import Contact from '../../components/mainPage/Contact/Contact.jsx';
+import { Footer } from '../../components/mainPage/Footer/Footer.jsx';
 import './style.css';
 
 export const MainPage = () => {
@@ -7,6 +8,7 @@ export const MainPage = () => {
         <main>
             <Banner />
             <Contact />
+            <Footer />
         </main>
     );
 };
