2759f08df9a79f09545670ef2c5255cb7e72818c
Tomasz <przadkatomasz.alt@gmail.com>
2022-01-21 19:02:59 +0100

    fix footer position

--- src/components/mainPage/Footer/Footer.jsx
+++ src/components/mainPage/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <>
-            <footer className='bg-light'>
+            <footer className='bg-light fixed-bottom'>
                 <p className='text-center text-muted py-3'>(c) Healthy Food 2022</p>
             </footer>
         </>
