a84f8003405defc8b787137a0252c6f949c3bf74
Tomasz <przadkatomasz.alt@gmail.com>
2022-01-22 01:01:57 +0100

    refactor routes so it now enumerates paths

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,3 @@
-import { MainPage, UserProfile } from './pages';
-import { PROFILE_TABS } from './components';
-import Recipes from './pages/recipes/Recipes';
-import Menus from './pages/menus/Menus';
-
 export const ROUTES_PATHS = {
     MAIN_PAGE: '/',
     RECIPES: '/recipes',
@@ -19,42 +14,8 @@ export const ROUTES_PATHS = {
     USER_FAVORITES: '/user-profile/favourites',
 };
 
-export const routes = [
-    {
-        path: ROUTES_PATHS.MAIN_PAGE,
-        element: <MainPage />,
-        key: 'main-page',
-    },
-
-    {
-        path: ROUTES_PATHS.USER_PROFILE,
-        element: <UserProfile tab={PROFILE_TABS.PROFILE_RECIPES} />,
-        key: 'user-profile',
-    },
-    {
-        path: ROUTES_PATHS.USER_RECIPES,
-        element: <UserProfile tab={PROFILE_TABS.PROFILE_RECIPES} />,
-        key: PROFILE_TABS.PROFILE_RECIPES,
-    },
-    {
-        path: ROUTES_PATHS.USER_MENUS,
-        element: <UserProfile tab={PROFILE_TABS.PROFILE_MENUS} />,
-        key: PROFILE_TABS.PROFILE_MENUS,
-    },
-    {
-        path: ROUTES_PATHS.USER_FAVORITES,
-        element: <UserProfile tab={PROFILE_TABS.PROFILE_FAVOURITES} />,
-        key: PROFILE_TABS.PROFILE_FAVOURITES,
-    },
-
-    {
-        path: ROUTES_PATHS.RECIPES,
-        element: <Recipes />,
-        key: 'recipes',
-    },
-    {
-        path: ROUTES_PATHS.MENUS,
-        element: <Menus />,
-        key: 'menus',
-    },
-];
+export const PROFILE_TABS = {
+    PROFILE_RECIPES: 'user-recipes',
+    PROFILE_MENUS: 'user-menus',
+    PROFILE_FAVOURITES: 'user-favourites',
+};
