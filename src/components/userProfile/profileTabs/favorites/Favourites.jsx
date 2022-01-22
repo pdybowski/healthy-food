e7e1889700d1dd86b61dd9286cc27e56cda0c6dd
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2022-01-22 12:11:40 +0100

    useState added to Favourites Component

--- src/components/userProfile/profileTabs/favorites/Favourites.jsx
+++ src/components/userProfile/profileTabs/favorites/Favourites.jsx
@@ -3,12 +3,25 @@ import { Button, Container, Row } from 'react-bootstrap';
 import RecipeTile from '../../../shared/tiles/recipeTile/RecipeTile.jsx';
 
 function Favourites() {
+    const [isFavRecipes, setIsFavRecipes] = React.useState(false);
+    const [isFavMenu, setIsFavMenu] = React.useState(false);
+
+    const recipesHandlerButton = () => {
+        setIsFavRecipes(true);
+        isFavMenu === false ? null : setIsFavMenu(false);
+    };
+
+    const menuHandlerButton = () => {
+        setIsFavMenu(true);
+        isFavRecipes === false ? null : setIsFavRecipes(false);
+    };
+
     return (
         <Container className='my-4'>
-            <Button variant='outline-info' className='me-3 mb-3'>
+            <Button variant='outline-info' className='me-3 mb-3' onClick={recipesHandlerButton}>
                 Recipes
             </Button>
-            <Button variant='outline-info' className='mb-3'>
+            <Button variant='outline-info' className='mb-3' onClick={menuHandlerButton}>
                 Menus
             </Button>
             <Row xs={1} md={2} xxl={4} className='g-4'>
