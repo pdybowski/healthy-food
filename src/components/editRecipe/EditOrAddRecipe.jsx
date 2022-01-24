6b3dded852268f1e4f81cd274f31c14894d0b70d
Justyna Gładysz <jus.gladysz@gmail.com>
2022-01-24 15:39:41 +0100

    Add form error handling

--- src/components/editRecipe/EditOrAddRecipe.jsx
+++ src/components/editRecipe/EditOrAddRecipe.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import ReactQuill from 'react-quill';
@@ -9,13 +9,37 @@ function EditRecipe() {
     const [recipe, setRecipe] = useState('');
     const [description, setDescription] = useState('');
     const [form, setForm] = useState({});
-    // const [errors, setErrors] = useState({});
+    const [errors, setErrors] = useState({});
 
     const setField = (field, value) => {
         setForm({
             ...form,
             [field]: value,
         });
+        if (errors[field])
+            setErrors({
+                ...errors,
+                [field]: null,
+            });
+    };
+
+    const findFormErrors = () => {
+        const { title, time } = form;
+        const newErrors = {};
+        if (!title || title === '') newErrors.title = 'Cannot be blank!';
+        else if (title.length > 60) newErrors.title = 'Title is too long!';
+        if (!time || time === '') newErrors.time = 'Cannot be blank!';
+        return newErrors;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newErrors = findFormErrors();
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+        } else {
+            alert('Success!');
+        }
     };
 
     return (
@@ -27,47 +51,64 @@ function EditRecipe() {
                     type='text'
                     placeholder='Enter recipe title'
                     onChange={(e) => setField('title', e.target.value)}
+                    isInvalid={!!errors.title}
                 />
+                <Form.Control.Feedback type='invalid'>{errors.title}</Form.Control.Feedback>
             </Form.Group>
             <Form.Group className='mb-3'>
                 <Form.Label>Tags</Form.Label>
                 <div className='d-flex flex-row'>
                     <Form.Control
+                        className='me-3'
                         type='text'
                         placeholder='Enter tag'
                         onChange={(e) => setField('tags', e.target.value)}
                     />
-                    <FontAwesomeIcon icon={faPlus} className='text-primary' />
+                    <Button variant='primary'>
+                        <FontAwesomeIcon icon={faPlus} />
+                    </Button>
                 </div>
             </Form.Group>
             <Form.Group className='mb-3'>
                 <Form.Label>Time to prepare [min]</Form.Label>
-                <Form.Control type='number' onChange={(e) => setField('time', e.target.value)} />
+                <Form.Control
+                    type='number'
+                    onChange={(e) => setField('time', e.target.value)}
+                    isInvalid={!!errors.time}
+                />
+                <Form.Control.Feedback type='invalid'>{errors.time}</Form.Control.Feedback>
             </Form.Group>
-            <Form.Group className='mb-3'>
+            <Form.Group className='mb-3 required'>
                 <Form.Label>Meal type</Form.Label>
-                <Form.Check type='checkbox' label='Breakfast' />
+                <Form.Check type='checkbox' defaultChecked label='Breakfast' />
                 <Form.Check type='checkbox' label='Lunch' />
                 <Form.Check type='checkbox' label='Dinner' />
             </Form.Group>
             <Form.Group className='mb-3'>
-                <Form.Label>Ingredients </Form.Label>
+                <Form.Label className='me-3'>Ingredients</Form.Label>
                 <div className='d-flex flex-row'>
-                    <Form.Control
-                        className='me-3'
-                        type='text'
-                        onChange={(e) => setField('ingredients-name', e.target.value)}
-                    />
-                    <Form.Control
-                        className='me-3'
-                        type='number'
-                        onChange={(e) => setField('ingredients-number', e.target.value)}
-                    />
-                    <Form.Select aria-label='pcs'>
+                    <FloatingLabel label='Name' className='me-3'>
+                        <Form.Control
+                            type='text'
+                            placeholder='Name'
+                            onChange={(e) => setField('ingredient-name', e.target.value)}
+                        />
+                    </FloatingLabel>
+                    <FloatingLabel label='Quantity' className='me-3'>
+                        <Form.Control
+                            type='number'
+                            placeholder='Quantity'
+                            onChange={(e) => setField('ingredients-number', e.target.value)}
+                        />
+                    </FloatingLabel>
+                    <Form.Select aria-label='pcs' className='me-3'>
                         <option value='kg'>kg</option>
                         <option value='ml'>ml</option>
                         <option value='pcs'>pcs</option>
                     </Form.Select>
+                    <Button variant='primary'>
+                        <FontAwesomeIcon icon={faPlus} />
+                    </Button>
                 </div>
             </Form.Group>
             <Form.Group className='mb-3'>
@@ -91,7 +132,7 @@ function EditRecipe() {
                     onChange={(e) => setField('people-number', e.target.value)}
                 />
             </Form.Group>
-            <Button variant='primary' type='submit'>
+            <Button variant='primary' type='submit' onClick={handleSubmit}>
                 Submit
             </Button>
         </Form>
