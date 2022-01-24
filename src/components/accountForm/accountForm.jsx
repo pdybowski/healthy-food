0b1e7f866173b988c0de2bf512d3988e262264b3
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2022-01-24 17:20:12 +0100

    working with styles

--- src/components/accountForm/accountForm.jsx
+++ src/components/accountForm/accountForm.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button'
+// import Button from 'react-bootstrap/Button';
+import './accountForm.css';
 
 const AccountForm = (props) => {
     return (
         <Modal {...props} size='lg' aria-labelledby='contained-modal-title-vcenter' centered>
-            <Modal.Header closeButton>
-                <Modal.Title id='contained-modal-title-vcenter'>Modal heading</Modal.Title>
-            </Modal.Header>
+            <Modal.Header closeButton></Modal.Header>
             <Modal.Body>
                 <h4>Centered Modal</h4>
                 <p>
@@ -15,10 +14,10 @@ const AccountForm = (props) => {
                     facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac,
                     vestibulum at eros.
                 </p>
+                <span className={`goBack_arrow`} onClick={props.onHide}>
+                    Go back
+                </span>
             </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide}>Close</Button>
-            </Modal.Footer>
         </Modal>
     );
 };
