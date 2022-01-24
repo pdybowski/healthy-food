504d53145915e6d17a47acbd1ffed09ccd0dce34
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2022-01-24 17:36:25 +0100

    Sign In Componenet created

---
+++ src/components/accountForm/SignIn/SignIn.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const SignIn = ({ header, onSubmit }) => {
+    return (
+        <div className='d-flex flex-column justify-content-around align-items-center'>
+            <h2 className='text-center'>{header}</h2>
+            <form
+                onSubmit={onSubmit}
+                className='d-flex flex-column justify-content-around align-items-center'
+            >
+                <div className='form-group'>
+                    <label htmlFor='email'>
+                        <input
+                            type='email'
+                            className='form-control'
+                            id='email'
+                            aria-describedby='emailHelp'
+                            placeholder='Enter email'
+                        />
+                    </label>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='password'>
+                        <input
+                            type='password'
+                            className='form-control'
+                            id='password'
+                            placeholder='Password'
+                        />
+                    </label>
+                </div>
+                <Button type='submit' className='btn btn-primary col-sm'>
+                    Sign In
+                </Button>
+            </form>
+            <div className='d-flex flex-column'>
+                <Button type='submit' className='btn btn-outline-light'>
+                    Forget your password?
+                </Button>
+                <Button type='submit' className='btn btn-outline-light'>
+                    {`You don't have an account?`}
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default SignIn;
--- src/components/accountForm/accountForm.jsx
+++ src/components/accountForm/accountForm.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
-// import Button from 'react-bootstrap/Button';
+import SignIn from './SignIn/SignIn';
+import Button from 'react-bootstrap/Button';
 import './accountForm.css';
 
 const AccountForm = (props) => {
     return (
         <Modal {...props} size='lg' aria-labelledby='contained-modal-title-vcenter' centered>
             <Modal.Header closeButton></Modal.Header>
-            <Modal.Body>
-                <h4>Centered Modal</h4>
-                <p>
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac
-                    facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac,
-                    vestibulum at eros.
-                </p>
-                <span className={`goBack_arrow`} onClick={props.onHide}>
+            <Modal.Body className='d-flex flex-column justify-content-around align-items-center'>
+                <SignIn header={'Sign In'} />
+                <Button type='submit' className='btn btn-outline-light' onClick={props.onHide}>
                     Go back
-                </span>
+                </Button>
             </Modal.Body>
         </Modal>
     );
