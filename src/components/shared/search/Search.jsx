92d6614bfcd64b8296ba978487c12c2eb0130e0b
patrycjastarzec <patryc.sc@gmail.com>
2022-01-26 19:47:53 +0100

    PS-46 Added logic for search box

--- src/components/shared/search/Search.jsx
+++ src/components/shared/search/Search.jsx
@@ -2,11 +2,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
-function Search() {
+function Search({ input: keyword, onChange: setKeyword }) {
     return (
         <div className='input-group mb-3'>
             <div className='form-outline'>
-                <input type='search' id='form1' className='form-control' />
+                <input
+                    value={keyword}
+                    type='search'
+                    id='form1'
+                    className='form-control'
+                    onChange={(e) => setKeyword(e.target.value)}
+                />
             </div>
             <button type='button' className='btn btn-primary'>
                 <FontAwesomeIcon icon={faSearch} />
--- src/pages/recipes/Recipes.jsx
+++ src/pages/recipes/Recipes.jsx
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import RecipeTile from '../../components/shared/tiles/recipeTile/RecipeTile';
 import Search from '../../components/shared/search/Search';
+import ApiQuery from '../../components/shared/ApiClass/ApiClass';
 
 function Recipes() {
+    const [recipes, setRecipes] = useState([]);
+    const [input, setInput] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setRecipes((await ApiQuery.get('recipes')).data);
+            } catch (err) {
+                if (err.response) {
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else {
+                    console.log(`Error: ${err.message}`);
+                }
+            }
+        };
+        fetchData();
+    }, []);
+
+    const updateInput = async (input) => {
+        setInput(input);
+    };
+
     return (
         <Container className='my-4'>
-            <Search />
+            <Search input={input} onChange={updateInput} />
             <Row xs={1} md={2} xxl={4} className='g-4'>
-                <RecipeTile isFavourite={false} isLoggedIn={false} isOwner={false} />
-                <RecipeTile isFavourite={false} isLoggedIn={false} isOwner={false} />
-                <RecipeTile isFavourite={false} isLoggedIn={false} isOwner={false} />
-                <RecipeTile isFavourite={false} isLoggedIn={false} isOwner={false} />
-                <RecipeTile isFavourite={false} isLoggedIn={false} isOwner={false} />
-                <RecipeTile isFavourite={false} isLoggedIn={false} isOwner={false} />
+                {recipes
+                    .filter((recipe) => {
+                        if (input === '') {
+                            return recipe;
+                        } else if (recipe.title.toLowerCase().includes(input.toLowerCase())) {
+                            return recipe;
+                        }
+                    })
+                    .map((recipe) => {
+                        return (
+                            <RecipeTile
+                                title={recipe.title}
+                                itemTags={recipe.tags}
+                                time={recipe.timeToPrepare}
+                                mealType={recipe.mealType}
+                                key={recipe.id}
+                                isFavourite={false}
+                                isLoggedIn={false}
+                                isOwner={false}
+                            />
+                        );
+                    })}
             </Row>
         </Container>
     );
