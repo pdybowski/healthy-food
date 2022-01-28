1c0d1d18413522d37b7c13db1d3b42c62e9f80c5
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2022-01-28 19:11:08 +0100

    Logic for Select Bars added

--- src/pages/upsertMenu/upsertMenu.jsx
+++ src/pages/upsertMenu/upsertMenu.jsx
@@ -9,9 +9,9 @@ import './upsertMenu.css';
 
 function UpsertRecipe() {
     const [tags, setTags] = useState([]);
-    const [menus, setMenus] = useState([]);
+    const [menus] = useState([]);
     const [recipes, setRecipes] = useState([]);
-    const [typeOfMeal, setTypeOfMeal] = useState('');
+    const [typeOfMeal, setTypeOfMeal] = useState('Breakfast');
 
     const days = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'];
     const mealType = ['Breakfast', 'Dinner', 'Supper', 'Snacks'];
@@ -23,16 +23,17 @@ function UpsertRecipe() {
 
         fetchData();
     }, []);
-    console.log(recipes);
 
     const handleDelete = (i) => {
         setTags(tags.filter((tag, index) => index !== i));
     };
+    console.log(recipes);
 
     const handleAddition = (tag) => {
         setTags([...tags, tag]);
     };
 
+    console.log(typeOfMeal);
     return (
         <Container className='my-4'>
             <h1>Create new menu</h1>
@@ -58,44 +59,52 @@ function UpsertRecipe() {
                             <Carousel.Item key={index} indicators='false' interval={null}>
                                 <h2 className={'text-center'}>{day}</h2>
                                 {menus > 0 ? <ul></ul> : null}
-                                <Form>
-                                    <Form.Group className='mb-3'>
-                                        <Form.Label className='me-3'>Meals</Form.Label>
-                                        <div className='d-flex flex-row'>
-                                            <Form.Select
-                                                aria-label='type-of-meal'
-                                                className='me-3'
-                                                onSelect={(e) => setTypeOfMeal(e.target.value)}
-                                            >
-                                                {mealType.map((meal, index) => {
+                                <Form.Group className='mb-3'>
+                                    <Form.Label className='me-3'>Meals</Form.Label>
+                                    <div className='d-flex flex-row'>
+                                        <Form.Select
+                                            aria-label='type-of-meal'
+                                            className='me-3'
+                                            onChange={(e) => {
+                                                e.preventDefault;
+                                                setTypeOfMeal(e.target.value);
+                                            }}
+                                        >
+                                            {mealType.map((meal, index) => {
+                                                return (
+                                                    <option value={meal} key={index}>
+                                                        {meal}
+                                                    </option>
+                                                );
+                                            })}
+                                        </Form.Select>
+                                        <Form.Select aria-label='meal' className='me-3'>
+                                            {recipes.map((recipe) => {
+                                                if (
+                                                    recipe.mealType.includes(
+                                                        typeOfMeal.toUpperCase()
+                                                    )
+                                                ) {
                                                     return (
-                                                        <option value={meal} key={index}>
-                                                            {meal}
+                                                        <option
+                                                            value={recipe.title}
+                                                            key={recipe.id}
+                                                        >
+                                                            {recipe.title}
                                                         </option>
                                                     );
-                                                })}
-                                            </Form.Select>
-                                            <Form.Select aria-label='meal' className='me-3'>
-                                                {typeOfMeal !== '' ? (
-                                                    recipes.map((recipe) => {
-                                                        recipe.mealType.filter();
-                                                    })
-                                                ) : (
-                                                    <option value='no-meal'>
-                                                        {'Choose a meal type'}
-                                                    </option>
-                                                )}
-                                            </Form.Select>
-                                        </div>
-                                        <Button
-                                            variant='primary'
-                                            type='submit'
-                                            className='w-25 my-3 button-custom'
-                                        >
-                                            Add meal to my menu
-                                        </Button>
-                                    </Form.Group>
-                                </Form>
+                                                }
+                                            })}
+                                        </Form.Select>
+                                    </div>
+                                    <Button
+                                        variant='primary'
+                                        type='button'
+                                        className='w-25 my-3 button-custom'
+                                    >
+                                        Add meal to my menu
+                                    </Button>
+                                </Form.Group>
                             </Carousel.Item>
                         );
                     })}
