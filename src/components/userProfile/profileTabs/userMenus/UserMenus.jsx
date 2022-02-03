45f9bf4497ca04224558d88360b31ba86338a633
Tomasz <przadkatomasz.alt@gmail.com>
2022-02-03 19:36:48 +0100

    move hardcoded menu tiles to array, spread array as dummy data in User Menus tab, add logic to handle menu removal

--- src/components/userProfile/profileTabs/userMenus/UserMenus.jsx
+++ src/components/userProfile/profileTabs/userMenus/UserMenus.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import MenuTile from '../../../shared/tiles/menuTile/MenuTile.jsx';
 import NewItemTile from '../../../shared/tiles/newItemTile/NewItemTile';
 
 function UserMenus() {
+    const menuList = [
+        {
+            id: '1',
+            isFavourite: false,
+            isLoggedIn: true,
+            isOwner: true,
+            handleSave: removeMenu,
+        },
+        {
+            id: '2',
+            isFavourite: false,
+            isLoggedIn: true,
+            isOwner: true,
+            handleSave: removeMenu,
+        },
+        {
+            id: '3',
+            isFavourite: true,
+            isLoggedIn: true,
+            isOwner: true,
+            handleSave: removeMenu,
+        },
+    ];
+    const [menus, setMenus] = useState(menuList);
+    function removeMenu(menuId) {
+        setMenus((menus) => menus.filter((menu) => menu.id !== menuId));
+    }
+
     return (
         <Container className='my-4'>
             <Row xs={1} md={2} xxl={4} className='g-4'>
                 <NewItemTile buttonLabel={'Add new menu'} />
-                <MenuTile isFavourite={false} isLoggedIn={true} isOwner={true} />
-                <MenuTile isFavourite={false} isLoggedIn={true} isOwner={true} />
-                <MenuTile isFavourite={true} isLoggedIn={true} isOwner={true} />
+                {menus.map((menu) => (
+                    // eslint-disable-next-line react/jsx-key
+                    <MenuTile {...menu} />
+                ))}
             </Row>
         </Container>
     );
