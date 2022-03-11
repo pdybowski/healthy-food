91c75a471c565c23cb20eaaf057659ee81eebd55
p.dybowski13 <p.dybowski13@gmail.com>
2022-03-11 20:51:34 +0100

    fix

--- src/components/accountForm/SignIn/SignIn.jsx
+++ src/components/accountForm/SignIn/SignIn.jsx
@@ -49,21 +49,12 @@ const SignIn = ({ header, newUserHandler, onFormSubmit, onLogIn }) => {
 
         if (!email || email === '') {
             newErrors.email = 'E-mail is required!';
-        }
-
-        if (!/\S+@\S+\.\S+/.test(form.email)) {
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
             newErrors.email = 'E-mail is invalid';
         }
 
         if (!password || password === '') {
             newErrors.password = 'Password is required!';
-        }
-
-        if (
-            dataUsers.filter((user) => user.email === email && user.password === password)
-                .length === 0
-        ) {
-            newErrors.password = 'Password is invalid';
         }
 
         return newErrors;
