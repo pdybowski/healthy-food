10cd9f620971e6d12df5f81000132b9b9d6e0267
Daniela Karwowska <daniela.karwowska@o2.pl>
2022-03-14 19:35:17 +0100

    fix email validation

--- src/Constants/localStorage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useState } from 'react';
-
-export default function Constants(props) {
-    localStorage.setItem(props.set);
-    localStorage.getItem(props.get);
-}
