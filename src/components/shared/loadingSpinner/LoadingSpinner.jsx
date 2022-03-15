ee36604836e2b8e42fa843a12233dd8c65f89e69
Iwona Gorbacz <i.gorbacz@gmail.com>
2022-03-15 11:45:24 +0100

    spinner with props

--- src/components/shared/loadingSpinner/LoadingSpinner.jsx
+++ src/components/shared/loadingSpinner/LoadingSpinner.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './loadingSpinner.css';
 
-const LoadingSpinner = () => {
-    return <div className='loader'></div>;
+const LoadingSpinner = (props) => {
+    return <div className='loader' style={{ color: props.color }}></div>;
 };
 
 export default LoadingSpinner;
+
+//  <LoadingSpinner color='#f2a22c' />   // in this way you can use this component with any color 
+
